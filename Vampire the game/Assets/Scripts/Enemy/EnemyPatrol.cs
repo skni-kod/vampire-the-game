429d4d61bcf6f32bb6c703cdbcc6e8220f7ae1c4
Arroyo75 <jszczepanski75@gmail.com>
2023-02-14 22:04:03 +0100

    Enemy Melee & Ranged Templates
    Two type of enemies nearly done, missing few sprites and hp system

---
+++ Vampire the game/Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [Header ("Patrol Points")]
+    [SerializeField] private Transform leftEdge;
+    [SerializeField] private Transform rightEdge;
+
+    [Header ("Enemy")]
+    [SerializeField] private Transform enemy;
+
+    [Header("Movement Parameters")]
+    [SerializeField] private float speed;
+    private Vector3 initScale;
+    private bool movingLeft;
+
+    [Header("Idle Behaviour")]
+    [SerializeField] private float idleDuration;
+    private float idleTimer;
+
+    [Header("Enemy Animator")]
+    [SerializeField] private Animator anim;
+
+    private void Awake()
+    {
+        initScale = enemy.localScale;
+    }
+
+    private void OnDisable()
+    {
+        anim.SetBool("moving", false);
+    }
+
+    private void Update()
+    {
+        if (movingLeft)
+        {
+            if (enemy.position.x >= leftEdge.position.x)
+                MoveInDirection(-1);
+            else
+            {
+                DirectionChange();
+            }
+        }
+        else
+        {
+            if (enemy.position.x <= rightEdge.position.x)
+            {
+                MoveInDirection(1);
+            }
+            else
+            {
+                DirectionChange();
+            }
+        }
+    }
+
+    private void DirectionChange()
+    {
+        anim.SetBool("moving", false);
+        movingLeft = !movingLeft;
+    }
+
+    private void MoveInDirection(int _direction)
+    {
+        anim.SetBool("moving", true);
+        //Face direction
+        enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * _direction, initScale.y, initScale.z);
+
+        //Move
+        enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed,
+            enemy.position.y, enemy.position.z);
+          
+    }
+}
