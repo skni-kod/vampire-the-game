ca2e789de68d85008c585c46c9a926a755c60043
Fabus <116647101+xFabusx@users.noreply.github.com>
2023-03-08 10:29:41 +0100

    shop

--- Vampire the game/Assets/Scripts/ShopManager.cs
+++ Vampire the game/Assets/Scripts/ShopManager.cs
@@ -9,6 +9,10 @@ public class ShopManager : MonoBehaviour
     public Upgrade[] upgrades;
     
     public GameObject ShopUI;
+    public Transform ShopContent;
+    public GameObject itemPrefab;
+    public PlayerMovement player;
+
 
    
     private void Awake()
@@ -23,6 +27,50 @@ public class ShopManager : MonoBehaviour
             Destroy(gameObject);
         }
         DontDestroyOnLoad(gameObject);
+    }
+    private void Start()
+    {
+        foreach (Upgrade upgrade in upgrades )
+        {
+            GameObject item = Instantiate(itemPrefab, ShopContent);
+
+            upgrade.itemRef = item;
+
+            foreach (Transform child in  item.transform)
+            {
+                if(child.gameObject.name == "Quantity")
+                {
+                    child.gameObject.GetComponent<Text>().text = upgrade.ToString();
+                   
+                }
+                else if (child.gameObject.name == "Cost")
+                {
+                    child.gameObject.GetComponent<Text>().text = "$" + upgrade.cost.ToString();
+
+                }
+                else if (child.gameObject.name == "Name")
+                {
+                    child.gameObject.GetComponent<Text>().text = "$" + upgrade.name;
+                }
+                else if (child.gameObject.name == "Image")
+                {
+                    child.gameObject.GetComponent<Image>().sprite = upgrade.image;
+                }
+
+            }
+            item.GetComponent<Button>().onClick.AddListener(() => { });
+        }
+       
+      
+    }
+    private void BuyUpgrade(Upgrade upgrade)
+    {
+        if (coins >= upgrade.cost)
+        {
+            coins -= upgrade.cost;
+            upgrade.quantity++;
+            upgrade.itemRef.transform.GetChild(0).GetComponent<Text>().text = upgrade.quantity.ToString();
+        }
     }
     public void ToggleShop()
     {
@@ -39,4 +87,6 @@ public class Upgrade
     public string name;
     public int cost;
     public Sprite image;
+    [HideInInspector] public int quantity;
+    [HideInInspector] public GameObject itemRef;
 }
